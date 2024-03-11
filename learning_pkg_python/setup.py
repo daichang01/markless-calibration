d915d7902bb09625c52ca988c10a98a81f91d121
daichang01 <18181985920@163.com>
2024-03-11 09:02:32 +0800

    进行ros2框架搭建

---
+++ learning_pkg_python/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'learning_pkg_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='daichang',
+    maintainer_email='18181985920@163.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'node_helloworld = learning_pkg_python.node_helloworld:main',
+            'node_helloworld_class = learning_pkg_python.node_helloworld_class:main',
+            'topic_pub_helloworld = learning_pkg_python.topic_helloworld_pub:main',
+            'topic_sub_helloworld = learning_pkg_python.topic_helloworld_sub:main'
+
+
+        ],
+    },
+)
