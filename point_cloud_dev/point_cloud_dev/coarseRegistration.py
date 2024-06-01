856c31ee7d759cba017cd60cf825c96b07804022
daichang01 <18181985920@163.com>
2024-06-01 18:35:31 +0800

    pca-平移-icp配准验证效果不错,接下来做自动识别边缘转点云

--- point_cloud_dev/point_cloud_dev/coarseRegistration.py
+++ point_cloud_dev/point_cloud_dev/coarseRegistration.py
@@ -19,13 +19,31 @@ def apply_pca(point_cloud):
     transformation = np.eye(4)
     transformation[:3, :3] = vh
     transformation[:3, 3] = mean
-    return vh, transformation
+    return transformation
 
 def align_point_cloud(point_cloud, transformation):
     # 使用 PCA 的结果对点云进行变换
     points = np.asarray(point_cloud.points)
-    points_transformed = np.dot(points - np.mean(points, axis=0), transformation.T)
+     # 将点扩展为齐次坐标
+    points_homogeneous = np.hstack((points, np.ones((points.shape[0], 1))))
+    # points_transformed = np.dot(points - np.mean(points, axis=0), transformation.T)
+    # 应用变换
+    points_transformed = np.dot(points_homogeneous, transformation.T)
+    # 只保留变换后的 x, y, z 坐标
+    points_transformed = points_transformed[:, :3]
     point_cloud.points = o3d.utility.Vector3dVector(points_transformed)
+    return point_cloud
+
+def compute_centroid(point_cloud):
+    #计算质心
+    points = np.asarray(point_cloud.points)
+    centroid = np.mean(points, axis=0)
+    return centroid
+
+def translate_point_cloud(point_cloud, vector):
+    #平移向量
+    translated_points = np.asarray(point_cloud.points) + vector
+    point_cloud.points = o3d.utility.Vector3dVector(translated_points)
     return point_cloud
 
 def fine_registration(source, target, threshold):
@@ -53,6 +71,7 @@ pc2 = load_point_cloud("/home/daichang/Desktop/ros2_ws/src/markless-calibration/
 pc3 = load_point_cloud("/home/daichang/Desktop/ros2_ws/src/markless-calibration/pcd/wait-to-reg/teethscan.txt")
 pc4 = load_point_cloud("/home/daichang/Desktop/ros2_ws/src/markless-calibration/pcd/wait-to-reg/teethtrue.txt")
 
+###################################################################### 原始点云 ##################################################################################
 pc1.paint_uniform_color([1, 0, 0])  # 红色表示源点云
 pc2.paint_uniform_color([0, 1, 0])  # 绿色表示目标点云
 # 添加坐标轴
@@ -76,17 +95,23 @@ vis.destroy_window()
 ############################################################# 粗配准  ##################################################
 
 # 应用 PCA 进行配准
-transformation1,trans1 = apply_pca(pc1)
-transformation2,trans2 = apply_pca(pc2)
+transformation1 = apply_pca(pc1)
+transformation2 = apply_pca(pc2)
 pc1_aligned = align_point_cloud(pc1, transformation1)
 pc2_aligned = align_point_cloud(pc2, transformation2)
-pc3_aligned = align_point_cloud(pc3, transformation1)
-pc4_aligned = align_point_cloud(pc4, transformation2)
+#计算平移向量
+centroid_pc1 = compute_centroid(pc1_aligned)
+centroid_pc2 = compute_centroid(pc2_aligned)
+translation_vector = centroid_pc2 - centroid_pc1
+pc1_aligned = translate_point_cloud(pc1_aligned, translation_vector)
+
+
 
 
 print("原始点云的 PCA 结果：")
-print(f"PCA1 结果：\n{transformation1}{trans1}")
-print(f"PCA2 结果：\n{transformation2}{trans2}")
+print(f"PCA1 结果：\n{transformation1}")
+print(f"PCA2 结果：\n{transformation2}")
+print(f"平移向量：\n{translation_vector}")
 
 
 # 评估粗配准结果
@@ -104,15 +129,13 @@ axis_pc2_aligned = create_local_axis(pc2_aligned)
 # 可视化粗配准结果
 pc1_aligned.paint_uniform_color([1, 0, 0])  # 红色表示源点云
 pc2_aligned.paint_uniform_color([0, 1, 0])  # 绿色表示目标点云
-pc3_aligned.paint_uniform_color([1, 1, 0])  # 红色表示源点云
-pc4_aligned.paint_uniform_color([0, 1, 1])  # 绿色表示目标点云
+
 
 vis = o3d.visualization.Visualizer()
 vis.create_window(window_name='粗配准结果', width=800, height=600)
 vis.add_geometry(pc1_aligned)
 vis.add_geometry(pc2_aligned)
-vis.add_geometry(pc3_aligned)
-vis.add_geometry(pc4_aligned)
+
 vis.add_geometry(axis_pc)
 vis.add_geometry(axis_pc1_aligned)
 vis.add_geometry(axis_pc2_aligned)
@@ -152,6 +175,7 @@ vis.destroy_window()
 
 #######################################################验证######################################################
 pc3_aligned = align_point_cloud(pc3, transformation1)
+pc3_aligned = translate_point_cloud(pc3_aligned, translation_vector)
 pc4_aligned = align_point_cloud(pc4, transformation2)
 axis_pc3_aligned = create_local_axis(pc3_aligned)
 axis_pc4_aligned = create_local_axis(pc4_aligned)
